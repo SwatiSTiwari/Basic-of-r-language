5aa94977f395f03a11b3b0d52486ee9a9083a41d
SwatiSTiwari <swatistiwati13@gmail.com>
2022-09-02 10:01:51 +0530

    added operator file

---
+++ operator.r
@@ -0,0 +1,40 @@
+> #======================================== operator in r =================================================
+
+######## assignment operator 
+> a<-5
+> 5->a
+> a<<-3
+> 3->>a
+> print (a)
+[1] 3
+> a
+[1] 3
+> ######### arithmetic operator
+> a<-10
+> b<-20
+> a+b
+[1] 30
+> b-a
+[1] 10
+> a*b
+[1] 200
+> b/a
+[1] 2
+> a^b
+[1] 1e+20
+
+
+> ########## comparison operator
+> a==b
+[1] FALSE
+> a>b
+[1] FALSE
+> a<b
+[1] TRUE
+> a!=b
+[1] TRUE
+> a>=b
+[1] FALSE
+> a<=b
+[1] TRUE
+
